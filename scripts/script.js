df85ac8c477b6f69a0341025f07f7a1188c5c4ba
Yuri <yuri.g.yoshida@gmail.com>
2022-10-11 13:15:13 -0300

    consertei o redirecionamento

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,34 @@
-function validaLogin(){
+/*function validaLogin(event){
     if((document.meuForm.login.value == "") && (document.meuForm.senha.value == "")){
         alert("Preencha os campos para continuar!");
             document.meuForm.login.focus();
+            event.preventDefault();
     } else if(document.meuForm.login.value == ""){
         alert("Email necessário para continuar!");
             document.meuForm.login.focus();
+            event.preventDefault();
     } else if(document.meuForm.senha.value == ""){
         alert("Senha necessária para continuar!");
             document.meuForm.senha.focus();
+            event.preventDefault();
     }
-}
+}*/
 
+function validaLogin(event) {
+    if((document.meuForm.login.value == "") && (document.meuForm.senha.value == "")){
+        alert("Preencha os campos para continuar!");
+            document.meuForm.login.focus();
+            event.preventDefault();
+    } else if(document.meuForm.login.value == ""){
+        alert("Email necessário para continuar!");
+            document.meuForm.login.focus();
+            event.preventDefault();
+    } else if(document.meuForm.senha.value == ""){
+        alert("Senha necessária para continuar!");
+            document.meuForm.senha.focus();
+            event.preventDefault();
+    }
+  }
+  
+  const meuForm = document.getElementById('formulario');
+  formulario.addEventListener('submit', validaLogin);
