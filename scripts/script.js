12b6ed436fdad270eb1002f2891fd1d117c70e91
Yuri <yuri.g.yoshida@gmail.com>
2022-10-11 14:33:58 -0300

    att dashboard

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,3 @@
-/*function validaLogin(event){
-    if((document.meuForm.login.value == "") && (document.meuForm.senha.value == "")){
-        alert("Preencha os campos para continuar!");
-            document.meuForm.login.focus();
-            event.preventDefault();
-    } else if(document.meuForm.login.value == ""){
-        alert("Email necessário para continuar!");
-            document.meuForm.login.focus();
-            event.preventDefault();
-    } else if(document.meuForm.senha.value == ""){
-        alert("Senha necessária para continuar!");
-            document.meuForm.senha.focus();
-            event.preventDefault();
-    }
-}*/
-
 function validaLogin(event) {
     if((document.meuForm.login.value == "") && (document.meuForm.senha.value == "")){
         alert("Preencha os campos para continuar!");
